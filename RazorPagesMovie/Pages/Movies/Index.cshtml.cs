17caa779c8ee33683794cffe48939e789299a548
ARİF AYDOĞAN <aydoganarif@yahoo.com>
2023-05-27 00:42:59 +0300

    AA

--- RazorPagesMovie/Pages/Movies/Index.cshtml.cs
+++ RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -7,15 +7,23 @@ namespace RazorPagesMovie.Pages.Movies
 {
     public class IndexModel : PageModel
     {
+        /* 
+         * Bu kısımda Dependency Injection yapılıyor.
+         * Yapıcı metot, RazorPagesMovieContext'i sayfaya ekliyor. 
+         */
         private readonly RazorPagesMovie.Data.RazorPagesMovieContext _context;
-
         public IndexModel(RazorPagesMovie.Data.RazorPagesMovieContext context)
         {
             _context = context;
         }
 
-        public IList<Movie> Movie { get;set; } = default!;
 
+        /* 
+         * Bu kısımda OnGet veya OnGetAsync metoduyla sayfa durumu başlatılır.
+         * Burada OnGetAsync metodu sayfa durumunu başlatırken bir Movie listesi döndürür ve görüntüler.
+         * Yani bu sayfaya bir GET isteği yapıldığında Movie listesi dönülüyor.
+         */
+        public IList<Movie> Movie { get; set; } = default!;
         public async Task OnGetAsync()
         {
             if (_context.Movie != null)
--- RazorPagesMovie/Program.cs
+++ RazorPagesMovie/Program.cs
@@ -1,31 +1,32 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using RazorPagesMovie.Data;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages(); //Razor Pages deste�i eklendi.
 builder.Services.AddDbContext<RazorPagesMovieContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("RazorPagesMovieContext") ?? throw new InvalidOperationException("Connection string 'RazorPagesMovieContext' not found.")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("RazorPagesMovieContext") ?? throw new Exception("Connection string 'RazorPagesMovieContext' not found.")));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseExceptionHandler("/Error"); 
+    app.UseHsts();   // Varsayılan HSTS değeri 30 gündür. Bunu production senaryoları için değiştirmek isteyebilirsiniz, bkz. https://aka.ms/aspnetcore-hsts
 }
 
-app.UseHttpsRedirection();  //HTTP isteklerini HTTPS'ye y�nlendirir.
+app.UseHttpsRedirection();  //HTTP isteklerini HTTPS'ye yönlendirir.
 
-app.UseStaticFiles();   //HTML, CSS, g�r�nt�ler ve JavaScript gibi statik dosyalar�n kullan�m�na olanak tan�r.
+app.UseStaticFiles();   //HTML, CSS, görüntüler ve JavaScript gibi statik dosyaların kullanımına olanak tan�r.
 
-app.UseRouting();  //Middleware i�lem hatt�na yol e�le�tirmesi yapar.
+app.UseRouting();  //Middleware işlem hattına yol eşleştirmesi yapar.
 
-app.UseAuthorization(); //Kullan�c�y� g�venli kaynaklara eri�mesi i�in yetkiler. Bu uygulama yetkilendirme kullanm�yor, bu nedenle bu sat�r kald�r�labilir.
+app.UseAuthorization(); //Kullanıcıya g�venli kaynaklara eri�mesi i�in yetkiler. Bu uygulama yetkilendirme kullanm�yor, bu nedenle bu sat�r kald�r�labilir.
 
-app.MapRazorPages();  //Sayfalar i�in Razor endpoint y�nlendirmesini yap�land�r�yor.
+app.MapRazorPages();  //Sayfalar için Razor endpoint yönlendirmesini yapılandırıyor.
 
 app.Run();
