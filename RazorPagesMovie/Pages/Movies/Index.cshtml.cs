83290e735d29bd2e34af2c5321655a69c035f198
ARİF AYDOĞAN <aydoganarif@yahoo.com>
2023-05-27 16:11:25 +0300

    AA

--- RazorPagesMovie/Pages/Movies/Index.cshtml.cs
+++ RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -1,5 +1,6 @@
-﻿
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Models;
 
@@ -17,19 +18,38 @@ namespace RazorPagesMovie.Pages.Movies
             _context = context;
         }
 
+        /* 
+         * Bu kısımda sayfaya tür ve isimle arama yapmamızı sağlayan kodlar eklendi.
+         * OnGet metodu içinde hangi Movie'lerin dönüleceği belirlenip sayfa oluşturuluyor.
+         */
+        [BindProperty(SupportsGet = true)]  //HTTP GET isteklerinin bağlanması için gereklidir.
+        public string? SearchString { get; set; }
+        public SelectList? Genres { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? MovieGenre { get; set; }
+        public IList<Movie> Movie { get; set; } = default!;
+
 
         /* 
          * Bu kısımda OnGet veya OnGetAsync metoduyla sayfa durumu başlatılır.
          * Burada OnGetAsync metodu sayfa durumunu başlatırken bir Movie listesi döndürür ve görüntüler.
          * Yani bu sayfaya bir GET isteği yapıldığında Movie listesi dönülüyor.
          */
-        public IList<Movie> Movie { get; set; } = default!;
         public async Task OnGetAsync()
         {
-            if (_context.Movie != null)
-            {
-                Movie = await _context.Movie.ToListAsync();
-            }
+            IQueryable<string> genreQuery = from m in _context.Movie orderby m.Genre select m.Genre;  //tüm türleri veritabanından alan bir LINQ sorgusudur.
+
+            var movies = from m in _context.Movie select m;
+
+            if (!string.IsNullOrEmpty(SearchString))
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+
+            if (!string.IsNullOrEmpty(MovieGenre))
+                movies = movies.Where(x => x.Genre == MovieGenre);
+
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());  //Türlerin SelectList'i, farklı türler yansıtılarak oluşturulur.
+            Movie = await movies.ToListAsync();
         }
     }
 }
